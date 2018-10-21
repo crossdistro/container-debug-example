a15a4cd912544495f199e6bc655b1899bdf854e0
Pavel Šimerda <pavlix@pavlix.net>
2018-10-21 02:21:45 +0200

    get overlay and netns in shape

--- containers.py
+++ containers.py
@@ -5,6 +5,7 @@ import os
 import sys
 import subprocess
 import tempfile
+import pyroute2
 
 ffi = cffi.FFI()
 ffi.cdef("""
@@ -12,47 +13,79 @@ int unshare(int flags);
 int mount(const char *source, const char *target,
           const char *filesystemtype, unsigned long mountflags,
           const void *data);
+int umount2(const char *target, int flags);
 int pivot_root(const char *new_root, const char *put_old);
 """)
 libc = ffi.dlopen("libc.so.6")
-
-coding = "utf-8"
 
 CLONE_FS = 0x00000200
 CLONE_NEWNS = 0x00020000
 CLONE_NEWNET = 0x40000000
 
-CLONE_DEFAULTS = CLONE_NEWNS | CLONE_NEWNET
-
-MS_PRIVATE = 0x40000
-MS_SHARED = 0x100000
+MS_MOVE = 0x2000
 MS_REC = 0x4000
+MS_PRIVATE = 0x40000
+MS_SLAVE = 0x80000
+MS_SHARED = 0x100000
 
 MS_DEFAULTS = 0
+
+def _mkdir(target):
+    os.makedirs(target, exist_ok=True)
 
 def _check(status):
     if status == -1:
         raise OSError(ffi.errno, os.strerror(ffi.errno))
     return status
 
-def unshare(flags=CLONE_DEFAULTS):
+def _encode(name):
+    if isinstance(name, str):
+        return name.encode("utf-8")
+    elif name is None:
+        return ffi.NULL
+    else:
+        return name
+
+
+def unshare(flags):
     return _check(libc.unshare(flags))
 
 # TODO: support ffi.NULL
 def mount(source, target, fstype, flags, data):
-    print(source, target, fstype, flags, data)
     return _check(libc.mount(
-        source.encode(coding),
-        target.encode(coding),
-        fstype.encode(coding),
+        _encode(source),
+        _encode(target),
+        _encode(fstype),
         flags,
-        data.encode(coding)))
+        _encode(data)))
+
+def unmount(target, *, recursive=False):
+    flags = 0
+    if recursive:
+        flags |= MS_REC
+    return _check(libc.umount2(_encode(target), flags))
+
+def mount_tmpfs(target, *, makedirs=True):
+    if makedirs:
+        _mkdir(target)
+    return mount(
+        "none",
+        target,
+        "tmpfs",
+        0,
+        "")
 
 def mount_overlay(target, *,
         lowerdir="/",
         upperdir,
         workdir,
-        flags=MS_DEFAULTS):
+        flags=MS_DEFAULTS,
+        makedirs=True):
+    if makedirs:
+        _mkdir(target)
+        _mkdir(lowerdir)
+        _mkdir(upperdir)
+        _mkdir(workdir)
     return mount(
         "overlay",
         target,
@@ -60,52 +93,93 @@ def mount_overlay(target, *,
         flags,
         f"lowerdir={lowerdir},upperdir={upperdir},workdir={workdir}")
 
-def mount_unshare(target):
-    return _check(libc.mount(
-        b"none",
-        target.encode("utf-8"),
-        ffi.NULL,
-        MS_REC | MS_PRIVATE,
-        ffi.NULL))
+def mount_unshare():
+    unshare(CLONE_NEWNS)
+    return mount(
+        "none",
+        "/",
+        None,
+        MS_REC | MS_SLAVE,
+        None)
 
-def mount_kernel_filesystems():
-    mount("none", "/proc", "proc", 0, "")
-    mount("none", "/sys", "sysfs", 0, "")
-
-def pivot_root(new_root, put_old):
-    #print(new_root, put_old)
+def pivot_root(new_root, put_old, *,
+        makedirs=True):
+    if makedirs:
+        _mkdir(new_root)
+        _mkdir(put_old)
     return _check(libc.pivot_root(
-        new_root.encode(coding),
-        put_old.encode(coding)))
+        _encode(new_root),
+        _encode(put_old)))
 
-def pivot_temporary_overlay():
+def run(command):
+    return subprocess.run(command, check=True)
+
+def pivot_temporary_overlay(*, base="/"):
     """High level API for overlay based debugging"""
 
     assert os.getuid() == 0
 
-    # TODO: Handle cleanup. This is unfortunately not trivial as the
-    # temporary directory cannot be removed while things are mounted.
-    tmp = tempfile.mkdtemp(prefix="python-container-debug.")
-    #print(tmp)
-    for d in "root", "upper", "lower", "work", "oldroot":
-        os.makedirs(f"{tmp}/{d}", exist_ok=True)
+    # Disassociate
+    mount_unshare()
 
-    #print(os.listdir(tmp))
-    new_root = f"{tmp}/root"
-    mount_overlay(new_root,
-            upperdir=f"{tmp}/upper",
-            workdir=f"{tmp}/work")
-    mount_unshare("/")
-    put_old = f"{tmp}/root/oldroot"
-    os.makedirs(put_old, exist_ok=True)
-    unshare()
-    pivot_root(new_root, put_old)
-    mount_kernel_filesystems()
+    # Create tmpfs in a static location to avoid spurious temporary
+    # directories that cannot be easily cleaned up. Everything else will be
+    # created under that tmpfs and dropped automatically.
+    tmp = "/run/pycoz"
+    mount_tmpfs(tmp)
 
-    return tmp
+    # Create the overlay.
+    new_root = "/run/pycoz/root"
+    mount_overlay(
+        target=new_root,
+        lowerdir=base,
+        upperdir="/run/pycoz/upper",
+        workdir="/run/pycoz/work")
+
+    # Switch root to the overlay.
+    old_root = "/oldroot"
+    pivot_root(new_root, new_root + old_root)
+    os.chdir("/root")
+
+    # Fix missing mounts
+    for path in "/dev", "/proc", "/sys":
+        mount("/oldroot" + path, path, None, MS_MOVE, None)
+
+    # Get rid of the old root
+    run(["umount", "--recursive", old_root])
+    os.rmdir(old_root)
+
+def netns_with_veth():
+    """High level API for netns based debugging"""
+
+    assert os.getuid() == 0
+
+    # Configure namespace and interfaces
+    ip = pyroute2.IPDB()
+    ip.create(kind="veth", ifname="pycoz0", peer="pycoz1")
+    ip.commit()
+
+    ns = pyroute2.IPDB(nl=pyroute2.NetNS("pycoz"))
+
+    with ip.interfaces.pycoz0 as veth:
+        veth.net_ns_fd = "pycoz"
+    with ns.interfaces.pycoz0 as veth:
+        veth.add_ip("192.168.0.1/24")
+        veth.up()
+    with ip.interfaces.pycoz1 as veth:
+        veth.add_ip("192.168.0.2/24")
+        veth.up()
+
+    ip.commit()
+    ip.release()
+    ns.release()
+
+    # Switch namespace
+    #unshare(CLONE_NEWNET)
+    pyroute2.netns.setns("pycoz")
 
 if __name__ == "__main__":
-    tmp = pivot_temporary_overlay()
-    os.environ["OVERLAY_TMP_DIRECTORY"] = tmp
+    pivot_temporary_overlay()
+    netns_with_veth()
     status = subprocess.run(sys.argv[1:] or ["mount"])
     exit(status.returncode)
--- debug.py
+++ debug.py
@@ -2,12 +2,30 @@
 
 import subprocess
 import os
+import ptrace.debugger
 
 import containers
 
 def run(command):
     return subprocess.run(command, shell=True, check=True)
 
-containers.temporary_overlay()
-run("touch /test")
-print(os.listdir(f"/"))
+containers.pivot_temporary_overlay()
+containers.netns_with_veth()
+
+command = ["ping", "-n", "192.168.0.2"]
+
+debugger = ptrace.debugger.PtraceDebugger()
+process = debugger.addProcess(ptrace.debugger.child.createChild(command, False), True)
+
+while True:
+    process.syscall()
+    #event = debugger.waitProcessEvent()
+    event = debugger.waitProcessEvent()
+    if isinstance(event, ptrace.debugger.ProcessExit):
+        break
+    elif isinstance(event, ptrace.debugger.ProcessSignal):
+        print(event.signum)
+        print(event.name)
+        print(process.syscall_state.event(ptrace.func_call.FunctionCallOptions()))
+
+debugger.quit()
